3765c1b0d92025624b43200905694122866eba5c
Patrick <patrsc@nubu.at>
2022-04-09 00:12:06 +0200

    Refactor

---
+++ julia/solver.jl
@@ -0,0 +1,54 @@
+module PackageSolver
+
+using UUIDs
+using Pkg
+using Pkg.Resolve
+
+const Fixed = Pkg.Resolve.Fixed
+const Graph = Pkg.Resolve.Graph
+const VersionSpec = Pkg.Versions.VersionSpec
+const VersionRange = Pkg.Versions.VersionRange
+const VersionBound = Pkg.Versions.VersionBound
+
+function graph(compat, uuid_to_name, reqs)
+    fixed = Dict{UUID,Fixed}()
+    verbose = false
+    julia_version = VERSION
+    g = Graph(compat, uuid_to_name, reqs, fixed, verbose, julia_version)
+    return g
+end
+
+function use_names(sol, uuid_to_name)
+    n = Dict{String, VersionNumber}()
+    for (u, v) in sol
+        n[uuid_to_name[u]] = v
+    end
+    return n
+end
+
+function range(string)
+    parts = split(string, " - ")
+    lo = parts[1]
+    hi = parts[2]
+    lower = VersionBound(VersionNumber(lo))
+    wildcards = length(findall("*", hi))
+    n = 3 - wildcards
+    vers = split(hi, ".")
+    if n == 0
+        higher = VersionBound()
+    elseif n == 1
+        higher = VersionBound(parse(Int, vers[1]))
+    elseif n == 2
+        higher = VersionBound(parse(Int, vers[1]), parse(Int, vers[2]))
+    elseif n == 3
+        higher = VersionBound(parse(Int, vers[1]), parse(Int, vers[2]), parse(Int, vers[3]))
+    end
+    return VersionRange(lower, higher)
+end
+
+function version_spec(string)
+    ranges = split(string, ", ")
+    return VersionSpec([range(r) for r in ranges])
+end
+
+end
--- julia/test.jl
+++ julia/test.jl
@@ -1,12 +1,10 @@
-using Pkg
-using Pkg.Resolve
 using UUIDs
+using Pkg.Resolve
 
-const Fixed = Pkg.Resolve.Fixed
-const Graph = Pkg.Resolve.Graph
-const VersionSpec = Pkg.Versions.VersionSpec
-const VersionRange = Pkg.Versions.VersionRange
-const VersionBound = Pkg.Versions.VersionBound
+include("solver.jl")
+
+version_spec = PackageSolver.version_spec
+VersionSpec = PackageSolver.VersionSpec
 
 a_uuid = UUIDs.uuid4()
 b_uuid = UUIDs.uuid4()
@@ -42,47 +40,11 @@ reqs = Dict{UUID,VersionSpec}(
     a_uuid => version_spec("1.0.0 - 1.0.0"),
     b_uuid => version_spec("1.0.0 - 1.0.0")
 )
-fixed = Dict{UUID,Fixed}()
-verbose = false
-julia_version = VERSION
-
-function range(string)
-    parts = split(string, " - ")
-    lo = parts[1]
-    hi = parts[2]
-    lower = VersionBound(VersionNumber(lo))
-    wildcards = length(findall("*", hi))
-    n = 3 - wildcards
-    vers = split(hi, ".")
-    if n == 0
-        higher = VersionBound()
-    elseif n == 1
-        higher = VersionBound(parse(Int, vers[1]))
-    elseif n == 2
-        higher = VersionBound(parse(Int, vers[1]), parse(Int, vers[2]))
-    elseif n == 3
-        higher = VersionBound(parse(Int, vers[1]), parse(Int, vers[2]), parse(Int, vers[3]))
-    end
-    return VersionRange(lower, higher)
-end
-
-function version_spec(string)
-    ranges = split(string, ", ")
-    return VersionSpec([range(r) for r in ranges])
-end
-
-function use_names(sol, uuid_to_name)
-    n = Dict{String, VersionNumber}()
-    for (u, v) in sol
-        n[uuid_to_name[u]] = v
-    end
-    return n
-end
 
 spec = version_spec("1.1.0 - 2.0.0, 5.6.7 - 8.*.*")
 
-g = Graph(compat, uuid_to_name, reqs, fixed, verbose, julia_version)
+g = PackageSolver.graph(compat, uuid_to_name, reqs)
 
 @time sol = resolve(g)
 
-use_names(sol, uuid_to_name)
+PackageSolver.use_names(sol, uuid_to_name)
