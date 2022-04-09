f2a5bb448161bd7d8dc7b6eeb3098d950b46f0a2
Patrick <patrsc@nubu.at>
2022-04-09 22:10:18 +0200

    Update test.jl to solve elm packages

--- julia/Solver/src/Solver.jl
+++ julia/Solver/src/Solver.jl
@@ -3,6 +3,7 @@ module Solver
 using UUIDs
 using Pkg
 using Pkg.Resolve
+using JSON
 
 const Fixed = Pkg.Resolve.Fixed
 const Graph = Pkg.Resolve.Graph
@@ -19,9 +20,9 @@ function graph(compat, uuid_to_name, reqs)
 end
 
 function use_names(sol, uuid_to_name)
-    n = Dict{String, VersionNumber}()
+    n = Dict{String, String}()
     for (u, v) in sol
-        n[uuid_to_name[u]] = v
+        n[uuid_to_name[u]] = string(v)
     end
     return n
 end
@@ -54,6 +55,60 @@ end
 function solve(g)
     sol = resolve(g)
     use_names(sol, g.data.uuid_to_name)
+end
+
+function read_elm_graph(file)
+    data = JSON.parsefile(file)
+    uuid_to_name = Dict{UUID,String}()
+    name_to_uuid = Dict{String,UUID}()
+    for (pkg_name, _) in data
+        u = UUIDs.uuid4()
+        uuid_to_name[u] = pkg_name
+        name_to_uuid[pkg_name] = u
+    end
+    compat = Dict{UUID,Dict{VersionNumber,Dict{UUID,VersionSpec}}}()
+    for (pkg_name, vers) in data
+        u = name_to_uuid[pkg_name]
+        compat[u] = Dict()
+        for (ver, specs) in vers
+            v = VersionNumber(ver)
+            compat[u][v] = convert_elm_specs(specs, name_to_uuid)
+        end
+    end
+    return compat, uuid_to_name
+end
+
+function convert_elm_specs(specs, name_to_uuid)
+    s = Dict{UUID,VersionSpec}()
+    for (pkg_name, ver_spec) in specs
+        u = name_to_uuid[pkg_name]
+        s[u] = elm_version_constraint_to_spec(ver_spec)
+    end
+    return s
+end
+
+function elm_version_constraint_to_spec(ver_spec)
+    if occursin(" <= v <= ", ver_spec)
+        lo, hi = split(ver_spec, " <= v <= ")
+    elseif occursin(" <= v < ", ver_spec)
+        lo, hi_bound = split(ver_spec, " <= v < ")
+        # 1.2.3 -> 1.2.2
+        # 1.2.0 -> 1.1.*
+        # 1.0.0 -> 0.*.*
+        major, minor, patch = [parse(Int, item) for item in split(hi_bound, ".")]
+        if patch > 0
+            hi = "$(major).$(minor).$(patch-1)"
+        elseif minor > 0  # patch == 0
+            hi = "$(major).$(minor-1).*"
+        elseif major > 0 # minor == 0 and patch == 0
+            hi = "$(major-1).*.*"
+        else
+            error("unknown higher version: '$hi_bound'")
+        end
+    else
+        error("unknown format: '$ver_spec'")
+    end
+    return version_spec("$lo - $hi")
 end
 
 end
--- julia/test.jl
+++ julia/test.jl
@@ -1,50 +1,23 @@
-using UUIDs
-using Pkg.Resolve
 using Solver
 
-version_spec = Solver.version_spec
-VersionSpec = Solver.VersionSpec
+# Read graph
+compat, uuid_to_name = Solver.read_elm_graph("graph.json")
 
-a_uuid = UUIDs.uuid4()
-b_uuid = UUIDs.uuid4()
-shared_uuid = UUIDs.uuid4()
-
-compat = Dict{UUID,Dict{VersionNumber,Dict{UUID,VersionSpec}}}(
-    a_uuid => Dict(
-        VersionNumber("1.0.0") => Dict(
-            shared_uuid => version_spec("2.0.0 - 3.*.*")
-        )
-    ),
-    b_uuid => Dict(
-        VersionNumber("1.0.0") => Dict(
-            shared_uuid => version_spec("3.0.0 - 5.*.*")
-        )
-    ),
-    shared_uuid => Dict(
-        VersionNumber("1.0.0") => Dict(),
-        VersionNumber("1.5.0") => Dict(),
-        VersionNumber("2.0.0") => Dict(),
-        VersionNumber("3.0.0") => Dict(),
-        VersionNumber("3.6.9") => Dict(),
-        VersionNumber("4.0.0") => Dict(),
-        VersionNumber("5.0.0") => Dict()
-    )
-)
-uuid_to_name = Dict{UUID,String}(
-    a_uuid => "a",
-    b_uuid => "b",
-    shared_uuid => "shared",
-)
-reqs = Dict{UUID,VersionSpec}(
-    a_uuid => version_spec("1.0.0 - 1.0.0"),
-    b_uuid => version_spec("1.0.0 - 1.0.0")
-)
-
-spec = version_spec("1.1.0 - 2.0.0, 5.6.7 - 8.*.*")
-
-g = Solver.graph(compat, uuid_to_name, reqs)
-
-@time sol = resolve(g)
-@time sol = Solver.solve(g)
-
-sol
+# Solve every version of every package
+solutions = Dict{String,Dict{String,Dict{String,String}}}()
+for (u, vers) in compat
+    name = uuid_to_name[u]
+    println("solving $name")
+    solutions[name] = Dict()
+    for (ver, _) in vers
+        v = string(ver)
+        reqs = Dict(u => Solver.version_spec("$v - $v"))
+        g = Solver.graph(compat, uuid_to_name, reqs)
+        sol = Dict()
+        try
+            sol = Solver.solve(g)
+        catch err
+        end
+        solutions[name][v] = sol
+    end
+end
